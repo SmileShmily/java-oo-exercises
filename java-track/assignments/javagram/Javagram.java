e64f055ac17c9553b8ddccce826df90875783cd7
SmileShmily <zyxjyaya@gmail.com>
2016-10-17 22:59:49 -0500

    class 7
    static methods and properties

--- java-track/assignments/javagram/Javagram.java
+++ java-track/assignments/javagram/Javagram.java
@@ -40,9 +40,17 @@ public class Javagram {
 		} while(picture == null);
 		
 		// TODO - prompt user for filter and validate input
-		
+		int filterID = displayFilterMenu(in);
 		// TODO - pass filter ID int to getFilter, and get an instance of Filter back 
-		BlueFilter filter = getFilter();			
+		Filter filter = null;
+		do {	
+			try {
+				filter = getFilter(filterID, in);			
+			} catch (Exception e) {
+				System.out.println("Inappropriate choice.");
+				filterID = displayFilterMenu(in);
+			}
+		} while (filter == null);
 
 		// filter and display image
 		Picture processed = filter.process(picture);
@@ -56,14 +64,32 @@ public class Javagram {
 		String fileName = in.next();
 		
 		// TODO - if the user enters the same file name as the input file, confirm that they want to overwrite the original
+		boolean overwrite = false;
+		File newFile = new File(dir + File.separator + fileName);
 		
 		if (fileName.equals("exit")) {
 			System.out.println("Image not saved");
 		} else {
-			String absFileName = dir + File.separator + fileName;
-			processed.save(absFileName);
-			System.out.println("Image saved to " + absFileName);
-		}	
+			while (newFile.exists() && !overwrite) {
+				System.out.println("File already exists. Do you wish to overwrite? (Y/N)");
+				char answer = in.next().charAt(0);
+				if (Character.toUpperCase(answer) == 'Y') {
+					System.out.println("Overwritting file...");
+					overwrite = true;
+				}
+				else {
+					System.out.println("Save image to (relative to " + dir + "):");
+					fileName = in.next();
+					newFile = new File(dir + File.separator + fileName);
+				}
+			}
+			
+			//String absFileName = dir + File.separator + fileName;
+			processed.save(newFile);
+			System.out.println("Image saved to " + newFile);
+		}
+		
+	
 		
 		// close input scanner
 		in.close();
@@ -71,11 +97,32 @@ public class Javagram {
 	
 	// TODO - refactor this method to accept an int parameter, and return an instance of the Filter interface
 	// TODO - refactor this method to thrown an exception if the int doesn't correspond to a filter
-	private static BlueFilter getFilter() {
-		
+	private static Filter getFilter(int filterID, Scanner in) {
 		// TODO - create some more filters, and add logic to return the appropriate one
-		return new BlueFilter();
-		
+		switch (filterID) {	
+			case 1: return new BlueFilter();
+			case 2: return new InvertFilter();
+			case 3: return new GrayscaleFilter();
+			case 4:
+				System.out.println("Enter brightness level:");
+				int level = in.nextInt();
+				return new BrightnessFilter(level);
+			case 5: return new BlurFilter();
+			case 6: return new FlipHorizontal();
+			case 7: return new FlipVertical();
+			default: throw new ArrayIndexOutOfBoundsException();
+		}	
 	}
-
+	
+	private static int displayFilterMenu(Scanner in) {
+		System.out.println("Please choose a filter:");
+		System.out.println("1. Blue Filter");
+		System.out.println("2. Invert Filter");
+		System.out.println("3. Grayscale Filter");
+		System.out.println("4. Brightness Filter");
+		System.out.println("5. Blur Filter");
+		System.out.println("6. Flip Horizontal");
+		System.out.println("7. Flip Vertical");
+		return in.nextInt();
+	}
 }
